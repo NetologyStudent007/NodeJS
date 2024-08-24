659f32ef27af4354b736057b2a2b36607b30250c
NetologyStudent007 <noxplayerafk001@gmail.com>
2024-08-24 03:22:16 +0300

    update

--- src/books/schemas/book.ts
+++ src/books/schemas/book.ts
@@ -1,28 +1,36 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document, Types } from 'mongoose';
+import { Document } from 'mongoose';
 import { IBook } from '../interfaces/book';
 
 export type BookDocument = Document<Book>;
 
-@Schema()
+@Schema({
+  toJSON: {
+    virtuals: true,
+    transform: (_doc, obj) => {
+      delete obj.__v;
+      delete obj._id;
+      return obj;
+    },
+  },
+  toObject: {
+    virtuals: true,
+  },
+})
 export class Book implements IBook {
-  private _id: Types.ObjectId;
-
   @Prop({ required: true })
   public title: string;
 
   @Prop({ required: true })
   public author: string;
 
-  //тут попытался сделать возможность работать с полем _id как с id но сериализовать пока не удалось так как хотелось бы
-
-  public get id(): string {
-    return this._id.toString();
-  }
-
-  public set id(id: string) {
-    this._id = new Types.ObjectId(id);
-  }
+  id: string;
 }
 
-export const BookSchema = SchemaFactory.createForClass(Book);
+const BookSchema = SchemaFactory.createForClass(Book);
+
+BookSchema.virtual<BookDocument>('id').get(function () {
+  return this._id;
+});
+
+export { BookSchema };
